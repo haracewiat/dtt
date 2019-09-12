f7a72900acbaabac4ee2df885170db62544ee9bf
Beata Haracewiat <b.haracewiat@gmail.com>
2019-09-12 23:32:52 +0200

    Split the app into seperate components
    Now the file App.vue contains the navbar, footer and router-view components.

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,22 @@
 <template>
-  <div id="app" class="container-fluid">
-    <div id="nav">
-      <span><router-link to="/">Home</router-link> | </span>
-      <span><router-link to="/about">About</router-link> | </span>
-      <span><router-link to="/random">Random</router-link> | </span>
-      <span>
-        <div class="dropdown">
-          <span class="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Categories
-          </span>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <router-link to="/grayscale" class="dropdown-item">Grayscale</router-link> |
-            <router-link to="/blur" class="dropdown-item">Blur</router-link>
-          </div>   <!-- End of .dropdown-menu -->
-        </div>   <!-- End of .dropdown -->
-      </span>
-    </div>   <!-- End of #nav -->
-    <router-view/>
+  <div id="app">
+    <AppNavbar></AppNavbar>
+    <router-view></router-view>
+    <AppFooter></AppFooter>
   </div>   <!-- End of #app -->
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import AppFooter from '@/components/AppFooter.vue';
+import AppNavbar from '@/components/AppNavbar.vue';
 
-#nav a, span {
-  font-weight: bold;
-  color: #2c3e50;
-}
+@Component({
+  components: {
+    AppFooter, AppNavbar,
+  },
+})
+export default class App extends Vue {
 
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
-</style>
+</script>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="nav">
+      <span><router-link to="/">Home</router-link> | </span>
+      <span><router-link to="/about">About</router-link> | </span>
+      <span><router-link to="/random">Random</router-link> | </span>
+      <span>
+        <div class="dropdown">
+          <span class="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Categories
+          </span>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <router-link to="/grayscale" class="dropdown-item">Grayscale</router-link> |
+            <router-link to="/blur" class="dropdown-item">Blur</router-link>
+          </div>   <!-- End of .dropdown-menu -->
+        </div>   <!-- End of .dropdown -->
+      </span>
+    </div>   <!-- End of #nav -->
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a, span {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+@Component
+export default class AppNavbar extends Vue {
+}
+</script> 
